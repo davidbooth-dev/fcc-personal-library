f9362ce73273a5bf87253dbb9d0cf02ab185164a
David Booth <67768956+davidbooth-dev@users.noreply.github.com>
2020-09-12 16:30:40 +0100

    front-end complete

--- public/client.js
+++ public/client.js
@@ -1,25 +1,67 @@
+
+
 $(document).ready(function () {
   var items = [];
   var itemsRaw = [];
 
+  function refreshBookList(data) {
+    let items = [];
+    $.each(data, function (i, val) {
+      var commenttext = val.commentcount === 1 ? ' comment' : ' comments';
+      items.push('<tr><td class="bookItem" id="' + i + '">' + val.title + '</td><td>' + val.commentcount + commenttext + '</td></tr>');
+      return (i !== 14);
+    });
+    if (items.length >= 15) {
+      items.push('<p>...and ' + (data.length - 15) + ' more!</p>');
+    }
+    return items;
+  }
+
+  function refreshCommentsList(data) {
+    let comments = [];
+    if (data.comments.length > 0) {
+      comments.push('<table id="comments">')
+      $.each(data.comments, function (i, val) {
+        comments.push('<tr><td>' + val + '</td><td>');
+        comments.push('<button class="btn btn-info delComment" id="' + data._id + '">Delete Comment</button>');
+        comments.push('</td></tr>');
+      });
+      comments.push('</table>')
+    }
+    comments.push('<form id="newCommentForm"><input style="width:300px" type="text" class="form-control" id="commentToAdd" name="comment" placeholder="New Comment"></form>');
+    comments.push('<div class="btnContainer"><button class="btn btn-info addComment" id="' + data._id + '">Add Comment</button>');
+    comments.push('<button class="btn btn-danger deleteBook" id="' + data._id + '">Delete Book</button></div>');
+
+    $('#detailComments').html(comments.join(''));
+  }
+
+  function updateBookList() {
+    $.getJSON('/api/books', function (data) {
+      itemsRaw = data;
+      if (data.length !== 0) {
+        items = refreshBookList(data);
+        $('.listWrapper').html(items.join(''));
+      }
+      else {
+        $('<p>No Books Found</p>').appendTo('#display')
+      };
+    });
+  }
+
   $.getJSON('/api/books', function (data) {
-    //var items = [];
     itemsRaw = data;
     if (data.length !== 0) {
-      items.push('<table>' + (data.length - 15) + ' more!</p>');
-      $.each(data, function (i, val) {
-        console.log('Data: ', i, val);
-        var commenttext = val.commentcount === 1 ? ' comment' : ' comments';
-        items.push('<li class="bookItem" id="' + i + '">' + val.title + ' - ' + val.commentcount + commenttext + '</li>');
-        return (i !== 14);
-      });
-      if (items.length >= 15) {
-        items.push('<p>...and ' + (data.length - 15) + ' more!</p>');
-      }
-      $('<ul/>', {
+      let items = refreshBookList(data);
+      let elements = [];
+      elements.push('<table class="listWrapper"><tr><th>Title</th><th>#Comments</th></tr>');
+      elements.push(items.join(''));
+      elements.push('</table');
+
+      $('#display').html(elements.join(''));
+      /*$('<table/>', {
         'class': 'listWrapper',
         html: items.join('')
-      }).appendTo('#display');
+      }).appendTo('#display');*/
     }
     else {
       $('<p>No Books Found</p>').appendTo('#display')
@@ -27,27 +69,29 @@ $(document).ready(function () {
   });
 
   var comments = [];
-  $('#display').on('click', 'li.bookItem', function () {
+  $('#display').on('click', 'td.bookItem', function () {
     $("#detailTitle").html('<b>' + itemsRaw[this.id].title + '</b> (id: ' + itemsRaw[this.id]._id + ')');
+    let id = itemsRaw[this.id]._id;
     $.getJSON('/api/books/' + itemsRaw[this.id]._id, function (data) {
-      comments = [];
-      $.each(data.comments, function (i, val) {
-        comments.push('<li>' + val + '</li>');
-      });
-      comments.push('<br><form id="newCommentForm"><input style="width:300px" type="text" class="form-control" id="commentToAdd" name="comment" placeholder="New Comment"></form>');
-      comments.push('<br><button class="btn btn-info addComment" id="' + data._id + '">Add Comment</button>');
-      comments.push('<button class="btn btn-danger deleteBook" id="' + data._id + '">Delete Book</button>');
-      $('#detailComments').html(comments.join(''));
+      refreshCommentsList(data[0]);
     });
   });
 
   $('#bookDetail').on('click', 'button.deleteBook', function () {
+    let id = this.id;
     $.ajax({
       url: '/api/books/' + this.id,
       type: 'delete',
       success: function (data) {
-        //update list
-        $('#detailComments').html('<p style="color: red;">' + data + '<p><p>Refresh the page</p>');
+        comments = [];
+        messages = [data, 'Select a book to see it\'s details and comments'];
+        $.each(messages, function (i, val) {
+          comments.push('<p>' + val + '</p>');
+        });
+        $('#detailTitle').html(comments.join(''));
+        $('#detailComments').html('');
+
+        updateBookList();
       }
     });
   });
@@ -60,15 +104,25 @@ $(document).ready(function () {
       dataType: 'json',
       data: $('#newCommentForm').serialize(),
       success: function (data) {
-        comments = [];
-        comments.unshift(newComment); //adds new comment to top of list
-        $.each(data.comments, function (i, val) {
-          comments.push('<li>' + val + '</li>');
-        });
-        comments.push('<br><form id="newCommentForm"><input style="width:300px" type="text" class="form-control" id="commentToAdd" name="comment" placeholder="New Comment"></form>');
-        comments.push('<br><button class="btn btn-info addComment" id="' + data._id + '">Add Comment</button>');
-        comments.push('<button class="btn btn-danger deleteBook" id="' + data._id + '">Delete Book</button>');
-        $('#detailComments').html(comments.join(''));
+        refreshCommentsList(data);
+        updateBookList();
+      }
+    });
+  });
+
+  $('#bookDetail').on('click', 'button.delComment', function () {
+    let comment;
+    let parent = this.parentNode;
+    if (parent) {
+      comment = $(parent).prev('td').text();
+    }
+    $.ajax({
+      url: '/api/books/' + this.id + '/' + comment,
+      type: 'delete',
+      dataType: 'json',
+      success: function (data) {
+        refreshCommentsList(data);
+        updateBookList();
       }
     });
   });
@@ -80,7 +134,13 @@ $(document).ready(function () {
       dataType: 'json',
       data: $('#newBookForm').serialize(),
       success: function (data) {
-        //update list
+        if (data.length !== 0) {
+          items = refreshList(data);
+          $('.listWrapper').html(items.join(''));
+        }
+        else {
+          $('<p>No Books Found</p>').appendTo('#display')
+        }
       }
     });
   });
@@ -92,8 +152,12 @@ $(document).ready(function () {
       dataType: 'json',
       data: $('#newBookForm').serialize(),
       success: function (data) {
-        //update list
+        $('<p>' + data + '</p>').appendTo('#display');
       }
     });
+  });
+
+  $('#refresh').click(function () {
+    window.location.reload(true);
   });
 });
