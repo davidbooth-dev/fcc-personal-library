b94a70be3a9952624ebe68a6caa457a3a8c05620
David Booth <67768956+davidbooth-dev@users.noreply.github.com>
2020-09-12 16:32:22 +0100

    updated to use promises - additional functions

--- modules/db-module.js
+++ modules/db-module.js
@@ -6,20 +6,34 @@ const dboptions = {
     useUnifiedTopology: true
 };
 
-const project = 'books';
+const VERBOSE = false;
+const PROJECT = 'books';
 
-// Setup database
-function connectToDB(done) {
-    MongoClient.connect(process.env.MONGO_URI, dboptions, function (err, client) {
-        if (err) {
-            console.log("Database error: " + err);
+let db;
+
+const log = function () {
+    if (VERBOSE) {
+        var arguments = Array.prototype.slice.call(arguments);
+        if (arguments.length > 0) {
+            let args = arguments.map(function (item) {
+                return item;
+            });
+            let msg = args.splice(' ');
+            console.log(msg);
         }
-        else {
-            console.log("Connected to database");
-            done(client);
-        }
-    });
+    }
 }
+
+// Connect to database
+MongoClient.connect(process.env.MONGO_URI, dboptions, function (err, client) {
+    if (err) {
+        log("Database error: " + err);
+    }
+    else {
+        db = client.db('nodedb');
+        log("Connected to database");
+    }
+});
 
 // DB Functions
 
@@ -28,142 +42,117 @@ function connectToDB(done) {
 const find = function (title, done) {
     let data;
     if (title) data = { title: title }
-    
-    connectToDB(function (dbclient) {
-        const client = dbclient
-        const db = client.db('nodedb');
 
-        db.collection(project).find(data).toArray(function (err, docs) {
-            if (err) done(null, [])
-            done(null, docs);
-        });
-    });
+    db.collection(PROJECT).find(data).toArray()
+        .then(docs => done(null, docs))
+        .catch(err => done(null, []));
 }
 
 const createOne = function (title, done) {
-    connectToDB(function (dbclient) {
-        const client = dbclient
-        const db = client.db('nodedb')
 
-        let data = { $ne: { title: title }, comments: [], commentcount: 0 }
-        db.collection(project).insertOne(data, function (err, doc) {
-            if (err) done(err, null);
-            else if(!doc) done(null, [])
-            else if (doc.insertedCount === 0 ) done(null, []);
-            else done(null, doc.ops);
-        });
-    });
+    db.collection(PROJECT).findOneAndUpdate(
+        { title: title },
+        {
+            $setOnInsert: { title: title, comments: [], commentcount: 0 }
+        },
+        { returnOriginal: false, upsert: true })
+        .then(doc => {
+            if (doc) {
+                if (!doc.value) done(null, []); // document created
+                else if (doc.value) done(null, doc.value); // document updated
+                //console.log(`Successfully updated document: ${updatedDocument}.`)
+            } else {
+                log("No document matches the provided query.")
+            }
+        })
+        .catch(err => done(err, null));
 }
 
 const deleteMany = function (done) {
-    connectToDB(function (dbclient) {
-        const client = dbclient;
-        const db = client.db('nodedb');
 
-        db.collection(project).deleteMany(function (err, result) {
+    db.collection(PROJECT).deleteMany()
+        .then(result => {
             let count = result.result.n;
-            if (err) done(err, null);
-            else if (count === 0) done({ failure: 'Could Not Delete Documents' }, null);
+            if (count === 0) done({ failure: 'Could Not Delete Documents' }, null);
             else done(null, 'Complete Delete Successful');
-        });
-    });
+        })
+        .catch(err => done(err, null) );
 }
 
 //Functions for route /api/books/:id
 const findById = function (id, done) {
     let data = id ? { _id: ObjectId(id) } : { _id: '0' }
-    
-    connectToDB(function (dbclient) {
-        const client = dbclient;
-        const db = client.db('nodedb');
 
-        db.collection(project).find(data).toArray(function (err, docs) {
-            if (err) done(null, []);
-            else done(null, docs);
-        });
-    });
+    db.collection(PROJECT).find(data).toArray()
+        .then(docs => done(null, docs))
+        .catch(err => done(null, []));
 }
 
 const addComment = function (id, comment, done) {
-    connectToDB(function (dbclient) {
-        const client = dbclient;
-        const db = client.db('nodedb');
 
-        let data = { _id: ObjectId(id) }
-      
-        db.collection(project).findOneAndUpdate(
-            data,
-            {
-                $addToSet: { $ne: { comments: comment } },
-                $inc: { commentcount: 1 }
-            },
-            { new: true },
-            (err, doc) => {
-                if (!doc.value) done('Could Not Add Comment', null);
-                else if (doc) done(null, doc.value);
+    db.collection(PROJECT).findOneAndUpdate(
+        { _id: ObjectId(id), comments: { $ne: comment } },
+        {
+            $addToSet: { comments: comment },
+            $inc: { commentcount: 1 }
+        },
+        { returnOriginal: false })
+        .then(doc => {
+            log('doc: ', doc);
+            if (doc) {
+                if (!doc.value) done('Could Not Add Comment', null); // comment already exists
+                else if (doc.value) done(null, doc.value); // comment added
+            } else {
+                log("No document matches the provided query.")
             }
-        )
-    });
+        })
+        .catch(err => done(err, null))
 }
 
 const deleteComment = function (id, comment, done) {
     if (!id) done(null, 'No ID Sent');
     else if (!comment) done(null, 'No Comment Sent');
     else {
-        connectToDB(function (dbclient) {
-            const client = dbclient
-            const db = client.db('nodedb');
 
-            let data = { _id: id }
-
-            db.collection(project).findOneAndUpdate(
-                data,
-                {
-                    $pull: { comments: { value: 'Scary for Kids' } },
-                    $inc: { commentcount: -1 }
-                },
-                { new: true },
-                (err, doc) => {
-                    if (!doc.value) done('Could Not Delete Comment', null);
-                    else if (doc) done(null, doc.value);
-                }
-            )
-        });
+        db.collection(PROJECT).findOneAndUpdate(
+            { _id: ObjectId(id), comments: comment },
+            {
+                $pull: { comments: comment },
+                $inc: { commentcount: -1 }
+            },
+            { returnOriginal: false })
+            .then(doc => {
+                log(doc);
+                if (!doc.value) done('Could Not Delete Comment', null);
+                else if (doc) done(null, doc.value);
+            })
+            .catch(err => done(err, null));
     }
 }
 
 const deleteAllComments = function (done) {
-    connectToDB(function (dbclient) {
-        const client = dbclient
-        const db = client.db('nodedb');
 
-        let data = { _id: id }
-        db.collection(project).findOneAndUpdate(
-            data,
-            {
-                $set: { comments: [], commentcount: 0 }
-            },
-            { new: true, multi: true },
-            (err, doc) => {
-                if (err) done(err, null);
-                else if (doc) done(null, 'Complete Delete Successful');
-            }
-        )
-    });
+    db.collection(PROJECT).findOneAndUpdate(
+        { _id: id },
+        {
+            $set: { comments: [], commentcount: 0 }
+        },
+        { returnOriginal: false, multi: true })
+        .then(doc => {
+            if (doc) done(null, 'Complete Delete Successful');
+        })
+        .catch(err => done(err, null));
 }
 
 const deleteOne = function (id, done) {
-    connectToDB(function (dbclient) {
-        const client = dbclient
-        const db = client.db('nodedb');
 
-        let data = { _id: ObjectId(id) }
-        db.collection(project).deleteOne(data, function (err, result) {
-            if (err) done(err, null);
-            else if (result.deletedCount === 0) done({ failure: 'Could Not Delete ' + data._id }, null);
+    let data = { _id: ObjectId(id) }
+    db.collection(PROJECT).deleteOne(data)
+        .then(docs => {
+            if (docs.deletedCount === 0) done({ failure: 'Could Not Delete ' + data._id }, null);
             else done(null, 'Delete Successful');
-        });
-    })
+        })
+        .catch(err => done(err, null));
 }
 
 exports.find = find;
